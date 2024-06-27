6730f38ae30dfd018ece5720bbbcc216f177c345
Andre Regino <reginopanai@gmail.com>
2024-06-26 21:17:36 -0600

    basic structure for main router

---
+++ routes/router.js
@@ -0,0 +1,26 @@
+/********************
+ * Require Statements
+ ***************/
+
+const express = require('express');
+const router = express.Router()
+const tripRouter = require('./tripRouter.js')
+const userRouter = require('./userRouter.js')
+
+
+/********************
+ * Middleware
+ ***************/
+
+
+
+
+
+/****************************************
+* Routes
+* **************************************/
+
+
+
+
+module.exports = router;
