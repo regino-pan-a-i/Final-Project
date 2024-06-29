a05b26073b7d5c15c63ffa16d328cf2483d671f0
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-06-29 10:28:17 -0600

    switched to mongodb, started getting trips routes,controller

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-const { model } = require('mongoose');
 
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-router.use('/user', require('./userRouter'));
+router.use('/trips', require('./tripRouter'));
 
 module.exports = router;
